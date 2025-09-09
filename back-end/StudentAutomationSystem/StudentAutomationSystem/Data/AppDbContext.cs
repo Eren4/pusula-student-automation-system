3557a5f155cbf3897ed1398f68e335d5ffdf8703
Eren4 <erendenizkusu@gmail.com>
2025-09-09 16:53:44 +0300

    Create DbContext

---
+++ back-end/StudentAutomationSystem/StudentAutomationSystem/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using YourNamespace.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Teacher> Teachers { get; set; }
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Grade> Grades { get; set; }
+}
