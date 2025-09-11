bfafb692e4608e7bd29d898a97cdd4a746657dd7
Eren4 <erendenizkusu@gmail.com>
2025-09-11 15:03:21 +0300

    Implement teacher dashboard skeleton

--- back-end/StudentAutomationSystem/StudentAutomationSystem/obj/Debug/net9.0/StudentAutomationSystem.AssemblyInfo.cs
+++ back-end/StudentAutomationSystem/StudentAutomationSystem/obj/Debug/net9.0/StudentAutomationSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentAutomationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+762aa7c8cb5f0c562066933e9c23bd7fbe740c64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5132ddd41e454c8196bec17101496aa27b5a21b")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentAutomationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentAutomationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
