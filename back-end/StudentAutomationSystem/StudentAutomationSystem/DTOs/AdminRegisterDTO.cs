8af2d2b4f2b9739b7f1ba533831a05f2859f4d54
Eren4 <erendenizkusu@gmail.com>
2025-09-11 12:15:01 +0300

    Implement Register post from frontend

--- back-end/StudentAutomationSystem/StudentAutomationSystem/DTOs/AdminRegisterDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace StudentAutomationSystem.DTOs;
-
-public class AdminRegisterDTO
-{
-    public string Nickname { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-}
--- front-end/student-frontend/src/components/Register.tsx
+++ front-end/student-frontend/src/components/Register.tsx
@@ -8,13 +8,39 @@ function Register() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [passwordConfirm, setPasswordConfirm] = useState('');
+    const [role, setRole] = useState('Student');
     const [error, setError] = useState('');
     const navigate = useNavigate();
+
+    const handleRegister = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if(password !== passwordConfirm) {
+            setError('Passwords do not match.');
+            return;
+        }
+
+        try {
+            const response = await axios.post('http://localhost:5000/api/auth/register', {
+                 name,
+                 surname,
+                 email,
+                 password,
+                 role
+            });
+            localStorage.setItem('userRole', response.data.role);
+            navigate("/dashboard");
+        }
+        catch (err) {
+            console.error(err);
+            setError('Registration failed. Please try again.');
+        }
+    };
 
     return (
         <div>
             <h2>Sign Up</h2>
-            <form onSubmit={console.log("Hello")}>
+            <form onSubmit={handleRegister}>
                 <div>
                     <label>Name:</label>
                     <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
@@ -41,13 +67,13 @@ function Register() {
                 </div>
 
                 <div>
-                    <input type='radio' id='student' name='role' value='Student' defaultChecked />
+                    <input type='radio' id='student' name='role' value='Student' checked={role === 'Student'} onChange={(e) => setRole(e.target.value)} />
                     <label htmlFor='student'>Student</label>
 
-                    <input type='radio' id='teacher' name='role' value='Teacher' />
+                    <input type='radio' id='teacher' name='role' value='Teacher' checked={role === 'Teacher'} onChange={(e) => setRole(e.target.value)} />
                     <label htmlFor='teacher'>Teacher</label>
-                    
-                    <input type='radio' id='admin' name='role' value='Admin' />
+
+                    <input type='radio' id='admin' name='role' value='Admin' checked={role === 'Admin'} onChange={(e) => setRole(e.target.value)} />
                     <label htmlFor='admin'>Admin</label>
                 </div>
 
